5b0ef936c8adb5f924ac22262c6f8d47e69ce536
Catherine Escobar <catescobar@Catherines-MacBook-Air-6.local>
2026-03-02 20:22:24 -0500

    changes to route.ts to handle errors

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,11 +4,19 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
+    const error = searchParams.get('error')
+
+    if (error) {
+        return NextResponse.redirect(`${origin}/login?error=${error}`)
+    }
 
     if (code) {
         const supabase = await createClient()
-        await supabase.auth.exchangeCodeForSession(code)
+        const { error: sessionError } = await supabase.auth.exchangeCodeForSession(code)
+        if (!sessionError) {
+            return NextResponse.redirect(`${origin}/protected`)
+        }
     }
 
-    return NextResponse.redirect(`${origin}/protected`)
+    return NextResponse.redirect(`${origin}/login`)
 }
